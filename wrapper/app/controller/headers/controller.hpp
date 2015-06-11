ce7faf1054f0771f61341c4079caf8d7e4399fd3
Luke Morrison <busx@dmv-exchange.com>
2015-06-11 07:29:30 -0400

    fixed mismatch dbconfig

--- wrapper/app/controller/headers/controller.hpp
+++ wrapper/app/controller/headers/controller.hpp
@@ -16,7 +16,7 @@
 
 class DXServer : public cppcms::application {
 	public:
-		DXServer(cppcms::service &srv) : cppcms::application(srv), dbconn("dbname=dmvexchange user=db password=f9a548add9f0007ca4071e06f04e3f81 hostaddr=127.0.0.1 port=5432") {
+		DXServer(cppcms::service &srv) : cppcms::application(srv), dbconn("dbname=dmvexchange user=db password=f9a548add9f0007ca4071e06f04e3f81 hostaddr=173.66.17.87 port=5432") {
 			std::cout << "[+] Threaded..." << std::endl;
 			/*
 				@METHOD - GET
